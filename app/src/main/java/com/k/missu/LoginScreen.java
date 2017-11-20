eaad5ed0b706e0f63e932ea3f2cb77a76b89e4d2
JH <kjh@JHui-MacBook-Pro.local>
2017-11-20 22:00:04 +0900

    add code

--- app/src/main/java/com/k/missu/LoginScreen.java
+++ app/src/main/java/com/k/missu/LoginScreen.java
@@ -2,12 +2,33 @@ package com.k.missu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginScreen extends AppCompatActivity {
 
+    TextView id, pw;
+    Button enter;
+    HttpConn conn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+
+        id = (TextView) findViewById(R.id.id);
+        pw = (TextView) findViewById(R.id.pw);
+        enter = (Button) findViewById(R.id.enter);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id_text = id.getText().toString();
+                String pw_text = pw.getText().toString();
+                String response = conn.request("http://18.216.36.241/missu/user_authorization.php",id_text,pw_text);
+
+            }
+        });
     }
 }
