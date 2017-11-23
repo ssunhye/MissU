8c997dc765758bec00cd1ba70430677dbb7d22a1
JH <kjh@JHui-MacBook-Pro.local>
2017-11-23 19:45:57 +0900

    add Code

---
+++ app/src/main/java/com/k/missu/MainScreen.java
@@ -0,0 +1,10 @@
+package com.k.missu;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kjh on 2017. 11. 23..
+ */
+
+public class MainScreen extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/k/missu/Parser.java
@@ -0,0 +1,8 @@
+package com.k.missu;
+
+/**
+ * Created by kjh on 2017. 11. 23..
+ */
+
+public class Parser {
+}
